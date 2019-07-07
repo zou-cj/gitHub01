4d838940f75639006425f233bc838e5a168db845
zoucj <1095768010@qq.com>
2019-07-07 11:37:46 +0800

    第四次提交代码，测试先pull解决冲突

--- src/main/java/GitHub01Test.java
+++ src/main/java/GitHub01Test.java
@@ -5,6 +5,7 @@ public class GitHub01Test {
         System.out.println("我正在测试本地项目与gitHub端的代码冲突，这是gitHub的代码");
         System.out.println("不出意外，我的人工合并应该是成功的");
         System.out.println("我正在测试本地项目与gitHub端的代码冲突但是我先pull避免冲突，这是idea的代码");
+        System.out.println("实话实说，我的上一次测试失败了，我试图再来一次，这是idea的代码");
     }
 
 }
