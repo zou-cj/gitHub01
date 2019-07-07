77b386ea4d107c4fa0c5e9f3ff1c1d4cdba14285
邹成军 <51846080+zou-cj@users.noreply.github.com>
2019-07-07 10:41:22 +0800

    update for conflict
    update for conflict

--- src/main/java/GitHub01Test.java
+++ src/main/java/GitHub01Test.java
@@ -1,6 +1,7 @@
 public class GitHub01Test {
     public static void main(String[] args) {
         System.out.println("我正在测试上传我的第一个GitHub项目");
+        System.out.println("我正在测试本地项目与gitHub端的代码冲突，这是gitHub的代码");
 
     }
 
